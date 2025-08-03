6c5ea8e8d213fd96154649bccdc58037355ed0c8
jaimeAlorg <jaime.alorg1998@gmail.com>
2025-08-03 17:33:30 +0200

    Implemented responsive nav bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBar } from "./components/task-bar/nav-bar";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [NavBar],
 })
 export class App {
 }
