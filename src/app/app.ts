1d70551f2e81dec8a80a44818703a222fc6d4cf6
jaimeAlorg <jaime.alorg1998@gmail.com>
2025-08-04 12:10:39 +0200

    feat(routing): Implemented Angular router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBar } from "./components/nav-bar/nav-bar";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [NavBar],
+  imports: [NavBar, RouterOutlet],
 })
 export class App {
 }
