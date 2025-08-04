c7fdc82bdd3dd73ef8878a585cefcdc22dc270e7
jaimeAlorg <jaime.alorg1998@gmail.com>
2025-08-04 12:21:27 +0200

    feat(nav-bar): Implemented routing in nav bar

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterModule],
   templateUrl: './nav-bar.html',
   styleUrls: ['./nav-bar.scss']
 })
-export class NavBar {
+export class NavBar implements OnInit, OnDestroy {
   isMobileMenuOpen: boolean = false;
+  private routerSubscription: Subscription = new Subscription();
+
+  ngOnInit() {
+    this.routerSubscription = this.router.events.subscribe(event => {
+      if (this.isMobileMenuOpen) {
+        this.isMobileMenuOpen = false;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.routerSubscription.unsubscribe();
+  }
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
+
+  constructor(private router: Router) { }
+
 }
