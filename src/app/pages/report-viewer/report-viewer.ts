e7b35cfdd753e4e6274850144d51dadf09d39e2c
jaimeAlorg <jaime.alorg1998@gmail.com>
2025-08-05 10:02:10 +0200

    feat(report-viewer): Finished implementing the UI of table viewer

--- src/app/pages/report-viewer/report-viewer.ts
+++ src/app/pages/report-viewer/report-viewer.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { ReportListCard } from '../../components/report-list-card/report-list-card';
+import { MatDivider } from "@angular/material/divider";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { Table } from '../../components/table/table';
 
 @Component({
   selector: 'app-report-viewer',
-  imports: [ReportListCard],
+  imports: [ReportListCard, MatDivider, MatButtonModule, MatIconModule, MatTableModule, Table],
   templateUrl: './report-viewer.html',
   styleUrl: './report-viewer.scss'
 })
 export class ReportViewer {
-
 }
